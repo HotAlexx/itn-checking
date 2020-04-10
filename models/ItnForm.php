e9ba4dc52c9b3b7e5031e967bc804b715c4cd084
HotAlex <a@hotalex.ru>
2020-04-10 13:28:40 +0300

    Validation ITN code

--- models/ItnForm.php
+++ models/ItnForm.php
@@ -46,10 +46,51 @@ class ItnForm extends Model
     {
         if (!$this->hasErrors()) {
 
-            if ($this->itnCode == '1') {
+            if (!$this->validate10chars($this->itnCode) && !$this->validate12chars($this->itnCode)) {
                 $this->addError($attribute, 'Введеный код не является ИНН.');
             }
         }
     }
+
+    protected function validate10chars($validated_itn)
+    {
+        if (preg_match('#([\d]{10})#', $validated_itn, $m)) {
+            $inn = $m[0];
+            $code10 = (($inn[0] * 2 + $inn[1] * 4 + $inn[2] *10 + $inn[3] * 3 +
+                        $inn[4] * 5 + $inn[5] * 9 + $inn[6] * 4 + $inn[7] * 6 +
+                        $inn[8] * 8) % 11 ) % 10;
+            if ($code10 == $inn[9]) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } else {
+            return false;
+        }
+    }
+
+    protected function validate12chars($validated_itn)
+    {
+        if (preg_match('#([\d]{12})#', $validated_itn, $m)) {
+            $inn = $m[0];
+            $code11 = (($inn[0] * 7 + $inn[1] * 2 + $inn[2] * 4 + $inn[3] *10 +
+                        $inn[4] * 3 + $inn[5] * 5 + $inn[6] * 9 + $inn[7] * 4 +
+                        $inn[8] * 6 + $inn[9] * 8) % 11 ) % 10;
+            $code12 = (($inn[0] * 3 + $inn[1] * 7 + $inn[2] * 2 + $inn[3] * 4 +
+                        $inn[4] *10 + $inn[5] * 3 + $inn[6] * 5 + $inn[7] * 9 +
+                        $inn[8] * 4 + $inn[9] * 6 + $inn[10]* 8) % 11 ) % 10;
+
+            if ($code11 == $inn[10] && $code12 == $inn[11]) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } else {
+            return false;
+        }
+    }
+
 
 }
