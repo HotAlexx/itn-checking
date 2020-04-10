bb73bae148d3c811f12de82ec0819a9add7b3c35
HotAlex <a@hotalex.ru>
2020-04-10 11:33:02 +0300

    First commit

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'AvEs1EBcu8asFUoj9L_23iI1cgm3h_Uf',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
